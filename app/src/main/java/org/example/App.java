5bbd84d468272dd025ff9bd5c4edfd2247eb783d
RoyalDev231 <138261519+RoyalDev231@users.noreply.github.com>
2025-09-03 21:27:01 -0500

    Add files via upload

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,21 @@
 package org.example;
 
-public class App {
-  public int getInput() {
-    // Implement your function here
-  }
+import java.util.Scanner;
 
-  public static void main(String[] args) {
-    // Test your function here in a driver program
-  }
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int chosen = InputValidator.getIntInRange(
+                -50,                         
+                499,                          
+                "Please enter a value",      
+                "Your value is invalid",       
+                scanner::nextLine,             
+                System.out::println            // output consumer
+        );
+
+        System.out.println();
+        System.out.println("The value chosen by the user is " + chosen);
+    }
 }
